81819952bf17909c77f8919e749214d30a4a8a1d
Edwin Pineda <edwin@Edwins-MBP.home>
2021-04-23 11:08:01 -0400

    list of categories and find cocktail by category

--- drink-recommender/src/components/Dropdown.js
+++ drink-recommender/src/components/Dropdown.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import RandomCocktail from "./RandomCocktail"
 import IngredientSearch from "./IngredientSearch"
+import CategoryCocktail from "./CategoryCocktail"
+import ListCategories from "./ListCategories"
 
 
 class Dropdown extends Component {
@@ -10,6 +12,8 @@ class Dropdown extends Component {
                 <div class="wrapper">
                     <RandomCocktail />
                     <IngredientSearch />
+                    <ListCategories />
+                    <CategoryCocktail />
                 </div>
             </div>
         )
